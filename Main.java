ac1ef2a092e05f1783dc3271f63d4aba463041da
balez4464 <balez4464@wrdsb.ca>
2020-09-14 15:24:27 +0000

    Created a prgram so user can calculate slope

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
-/**
+import java.util.Scanner;
+
+/**This program will calculate the slope of the users choosing
  *
- * @author 
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -10,6 +12,30 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    // tells and allows user to input first x point
+    System.out.print("Please enter in the x coordinate of the first point");
+
+    double xa = input.nextDouble();
+
+    // tells and allows user to input first y point
+    System.out.print("Please enter in the y coordinate of the first point");
+
+    double ya = input.nextDouble();
+
+    //tells and allows user to input second x point
+    System.out.println("Please enter in the x coordinate of the second point");
+
+    double xb = input.nextDouble();
+
+    //tells and allows user to input second y point
+    System.out.println("Please enter in the y coordinate of the second point");
+
+    double yb = input.nextDouble();
+
+    //Tells and calculates users slope
+    double slope = (yb - ya)/(xb - xa);
     
+    System.out.println("The slope of your line would be " + slope + ".");
   }
 }
